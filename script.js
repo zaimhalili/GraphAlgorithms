bf8dcc2b7c5b9c34b9ebafdde43be1ac1704e205
Zaim Halili <halilizaim@gmail.com>
2026-05-20 19:15:38 +0200

    feat: implement Dijkstra's algorithm and update adjacency matrix

--- script.js
+++ script.js
@@ -5,47 +5,16 @@ class Graph {
 }
 
 let matrix = [
-    [
-        0,
-        5,
-        100,
-        7,
-        100
-    ],
-    [
-        5,
-        0,
-        4,
-        9,
-        1
-    ],
-    [
-        100,
-        4,
-        0,
-        100,
-        12
-    ],
-    [
-        7,
-        9,
-        100,
-        0,
-        8
-    ],
-    [
-        100,
-        1,
-        12,
-        8,
-        0
-    ]
+    [[1, 4], [2, 8]],
+    [[0, 4], [4, 6], [2, 3]],
+    [[0, 8], [3, 2], [1, 3]],
+    [[2, 2], [4, 10]],
+    [[1, 6], [3, 10]]
 ];
-
-selectAlgorithm("Dijkstra");
 
 let row = 0;
 let column = 0;
+let depth = 0;
 
 function createMapBlocks(numberOfBlocks) {
     for (let i = 0; i < numberOfBlocks; i++) {
@@ -57,16 +26,104 @@ function createMapBlocks(numberOfBlocks) {
         if (currentDiv) {
             currentDiv.before(newDiv);
         }
-        newDiv.innerText = matrix[row][column].toString();
-        column++;
-        if (column == 5) {
-            row++;
-            column = 0;
-        }
+        // newDiv.innerText = matrix[row][column].toString();
+        // column++;
+        // if (column == 5) {
+        //     row++;
+        //     column = 0;
+        // }
     }
 }
 
 createMapBlocks(25);
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(item) {
+        this.heap.push(item);
+        this._bubbleUp();
+    }
+
+    pop() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._bubbleDown();
+        return top;
+    }
+
+    _bubbleUp() {
+        let i = this.heap.length - 1;
+        while (i > 0) {
+            let p = Math.floor((i - 1) / 2);
+            if (this.heap[p][0] <= this.heap[i][0]) break;
+            [this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+            i = p;
+        }
+    }
+
+    _bubbleDown() {
+        let i = 0;
+        const n = this.heap.length;
+        while (true) {
+            let l = 2 * i + 1, r = 2 * i + 2, smallest = i;
+
+            if (l < n && this.heap[l][0] < this.heap[smallest][0]) smallest = l;
+            if (r < n && this.heap[r][0] < this.heap[smallest][0]) smallest = r;
+
+            if (smallest === i) break;
+            [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+            i = smallest;
+        }
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}
+//Driver Code Ends
+
+function dijkstra(adj, src) {
+
+    let V = adj.length;
+
+    // Min-heap (priority queue) storing pairs of (distance, node)
+    let pq = new MinHeap();
+
+    let dist = Array(V).fill(Number.MAX_SAFE_INTEGER);
+
+    // Distance from source to itself is 0
+    dist[src] = 0;
+    pq.push([0, src]);
+
+    // Process the queue until all reachable vertices are finalized
+    while (!pq.isEmpty()) {
+        let [d, u] = pq.pop();
+
+        // If this distance not the latest shortest one, skip it
+        if (d > dist[u]) continue;
+
+        // Explore all neighbors of the current vertex
+        for (let [v, w] of adj[u]) {
+
+            // If we found a shorter path to v through u, update it
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push([dist[v], v]);
+            }
+        }
+    }
+
+    // Return the final shortest distances from the source
+    return dist;
+}
+
+function astar() {
+
+}
 
 function selectAlgorithm(name) {
     document.getElementById("dijkstra").classList.remove("selectedButton");
@@ -74,18 +131,13 @@ function selectAlgorithm(name) {
 
     if (name === "Dijkstra") {
         document.getElementById("dijkstra").classList.add("selectedButton");
-        Dijkstra();
+        dijkstra(matrix, 0);
     } else if (name === "Astar") {
         document.getElementById("astar").classList.add("selectedButton");
-        Astar();
+        astar();
     }
 }
 
-function Dijkstra() {
-
-}
-
-function Astar() {
-
-}
+let result = dijkstra(matrix, 4);
+console.log(result);
 
